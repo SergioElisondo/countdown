6a53288c62244bb146836c0df421bffc0453568f
Sergio Elisondo <sergioelisondo@gmail.com>
2021-02-09 02:54:47 -0800

    this is a test countdown

---
+++ js/main1.js
@@ -0,0 +1,28 @@
+// Set the date we're counting down to
+let countDownDate = new Date("May 03, 2023 01:00:00").getTime();
+
+// Update the count down every 1 second
+let x = setInterval(function() {
+
+  // Get today's date and time
+  let now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  let distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element of choice with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "Welcome to <strong>nebula</strong>carina";
+  }
+}, 1000);
